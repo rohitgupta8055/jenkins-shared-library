7af5d52a8d18e169bcb446488aaf745ec5857dde
ROHIT KUMAR <97289743+rohitgupta8055@users.noreply.github.com>
2026-05-17 13:24:19 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,33 @@
-def call(String Project, String ImageTag, String dockerHubUser){
-    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-        echo "${env.dockerHubUser} ${env.dockerHubPass}"
-        sh "docker login -u ${dockerHubUser -p ${env.dockerHubPass}"
-    }
-    sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+def call(String project, String imageTag, String dockerHubUser){
+
+        echo '''
+        ██████╗     ██╗   ██╗    ███████╗    ██╗  ██╗
+        ██╔══██╗    ██║   ██║    ██╔════╝    ██║  ██║
+        ██████╔╝    ██║   ██║    ███████╗    ███████║
+        ██╔═══╝     ██║   ██║    ╚════██║    ██╔══██║
+        ██║          ╚██████╔╝    ███████║    ██║  ██║
+        ╚═╝           ╚═════╝     ╚══════╝    ╚═╝  ╚═╝
+        '''
+    
+        withCredentials([usernamePassword(
+            credentialsId: 'dockerHubCred',
+            passwordVariable: 'dockerHubPass',
+            usernameVariable: 'dockerHubUser')]) {
+    
+            echo "Docker Login Successful for User: ${dockerHubUser}"
+    
+            sh "docker login -u ${dockerHubUser} -p ${env.dockerHubPass}"
+        }
+    
+        sh "docker push ${dockerHubUser}/${project}:${imageTag}"
+    
+        echo '''
+        ███████╗    ██╗   ██╗     ██████╗     ██████╗    ███████╗
+        ██╔════╝    ██║   ██║    ██╔════╝    ██╔════╝    ██╔════╝
+        ███████╗    ██║   ██║    ██║         ██║         █████╗
+        ╚════██║    ██║   ██║    ██║         ██║         ██╔══╝
+        ███████║    ╚██████╔╝    ╚██████╗    ╚██████╗    ███████╗
+        ╚══════╝     ╚═════╝      ╚═════╝     ╚═════╝    ╚══════╝
+        '''
+    
 }
